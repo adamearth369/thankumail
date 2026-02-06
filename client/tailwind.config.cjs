dd42c070a7bd4b1cda495228289fb546d111136f
adamearth369 <newstartmedia369@gmail.com>
2026-02-06 22:05:46 +0000

    fix: correct tailwind postcss plugin + content paths

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "../shared/**/*.{js,ts,jsx,tsx}",
+    "./client/index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+    "./server/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
