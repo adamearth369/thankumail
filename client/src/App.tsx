4ee8fa6b947f96c84b2d59935c12705ea7c657e2
Adam Dodds <newstartmedia369@gmail.com>
2026-02-17 22:35:36 -0500

    ui: wire login routes (fix build imports)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,29 @@
+// WHERE TO PASTE: client/src/App.tsx
+// ACTION: Full file replacement (paste exactly)
+
 import { Switch, Route } from "wouter";
+
 import Home from "./pages/Home";
 import Claim from "./pages/Claim";
 import TurnstileTool from "./pages/turnstile-tool";
-import Login from "./pages/Login";
-import AuthConsume from "./pages/AuthConsume";
 import NotFound from "./pages/not-found";
+
+import Login from "./Login";
+import AuthConsume from "./AuthConsume";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/claim/:publicId" component={Claim} />
+
+      {/* Registered user flow */}
       <Route path="/login" component={Login} />
       <Route path="/auth/consume" component={AuthConsume} />
+
+      {/* Tools */}
       <Route path="/tools/turnstile" component={TurnstileTool} />
+
       <Route component={NotFound} />
     </Switch>
   );
