cfc3235f2e21132ced56b7414648bb8bbcf0a0f6
Adam Dodds <newstartmedia369@gmail.com>
2026-02-17 22:00:54 -0500

    auth: add login + magic link consume pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
+// WHERE TO PASTE: client/src/App.tsx
+// ACTION: Full file replacement (paste exactly)
+
 import { Switch, Route } from "wouter";
 import Home from "./pages/Home";
 import Claim from "./pages/Claim";
 import TurnstileTool from "./pages/turnstile-tool";
 import NotFound from "./pages/not-found";
+import Login from "./pages/Login";
+import AuthConsume from "./pages/AuthConsume";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/auth/consume" component={AuthConsume} />
       <Route path="/claim/:publicId" component={Claim} />
       <Route path="/tools/turnstile" component={TurnstileTool} />
       <Route component={NotFound} />
