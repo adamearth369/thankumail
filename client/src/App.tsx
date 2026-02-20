0e0ef04ed97365c8a2ba4488341e7b74b250a543
Adam Dodds <newstartmedia369@gmail.com>
2026-02-20 13:57:45 -0500

    auth: add google oauth route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,3 @@
-// WHERE TO PASTE: client/src/App.tsx
-// ACTION: Full file replacement
-
 import { Switch, Route } from "wouter";
 
 import Home from "./pages/Home";
@@ -8,9 +5,9 @@ import Claim from "./pages/Claim";
 import TurnstileTool from "./pages/turnstile-tool";
 import NotFound from "./pages/not-found";
 
-// FIX: correct folder (Render Linux build is case-sensitive)
 import Login from "./pages/Login";
 import AuthConsume from "./pages/AuthConsume";
+import AuthGoogle from "./pages/AuthGoogle";
 
 export default function App() {
   return (
@@ -18,11 +15,10 @@ export default function App() {
       <Route path="/" component={Home} />
       <Route path="/claim/:publicId" component={Claim} />
 
-      {/* Registered user flow */}
       <Route path="/login" component={Login} />
       <Route path="/auth/consume" component={AuthConsume} />
+      <Route path="/auth/google" component={AuthGoogle} />
 
-      {/* Tools */}
       <Route path="/tools/turnstile" component={TurnstileTool} />
 
       <Route component={NotFound} />
